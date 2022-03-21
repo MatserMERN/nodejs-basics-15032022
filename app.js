23b6a5631d8ef0f86717f6fa72f36712678b4db4
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-03-21 11:32:46 +0530

    added basic node files

---
+++ app.js
@@ -0,0 +1,23 @@
+function addition(a, b){
+    return a + b
+}
+
+console.log(addition(10,20))
+
+let multiply = function(a, b){
+    return a * b
+}
+
+console.log(multiply(10,20))
+
+const sayHello = () => {
+    return "Welcome to the world of NodeJS"
+}
+
+console.log(sayHello())
+
+const callMe = () => 'Did you call me ?'
+
+console.log(callMe())
+
+console.log('Hey, autosaved already ?')
